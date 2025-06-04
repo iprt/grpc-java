acac91610a5f502f901f5a513bfd490c15f886a8
zhenjie zhu <tech@intellij.io>
2025-06-04 13:19:23 +0800

    🩹 fix: simplify Address.from() implementation
    Refactored the `Address.from()` method by simplifying the ternary operation for better readability and consistency. This maintains the logic while improving the overall code clarity.

--- springboot/proto/src/main/java/org/iproute/grpc/boot/proto/context/Address.java
+++ springboot/proto/src/main/java/org/iproute/grpc/boot/proto/context/Address.java
@@ -59,13 +59,9 @@ public class Address {
      * @return The Address object created from the SocketAddress, or an unknown Address if the SocketAddress is null or not an instance of InetSocketAddress.
      */
     public static Address from(SocketAddress socketAddress, boolean local) {
-        return Objects.isNull(socketAddress)
-                ? unknown(local)
-                :
-                (
-                        (socketAddress instanceof InetSocketAddress address)
-                                ? Address.builder().host(address.getHostString()).port(address.getPort()).build()
-                                : unknown(local)
+        return Objects.isNull(socketAddress) ? unknown(local) :
+                ((socketAddress instanceof InetSocketAddress addr)
+                        ? Address.builder().host(addr.getHostString()).port(addr.getPort()).build() : unknown(local)
                 );
 
     }
